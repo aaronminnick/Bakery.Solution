47b2172980834377a8eabec9ae68e4a5bfdb3b59
Aaron Minnick <abminnick@gmail.com>
2022-01-14 10:09:48 -0800

    home page lists treats and flavors

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
+using Bakery.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Library.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly BakeryContext _db;
+
+    public HomeController(BakeryContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.Treats = _db.Treats.ToList();
+      ViewBag.Flavors = _db.Flavors.ToList();
       return View();
     }
   }
